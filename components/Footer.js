498d89817b7c7ce6ed608b9818a1ea8ce0c958e6
mt210621 <michaellgx@qq.com>
2021-11-20 19:59:46 +0800

    development...

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Footer = ()=>(
+    <div className="footer-div">
+        <div>系统由 React+Node+Ant Desgin驱动 </div>
+        <div>JSPang.com</div>
+    </div>
+)
+
+export default Footer
---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+import '../styles/globals.css'
+import '../styles/components/header.css'
+import '../styles/pages/comm.css'
+import '../styles/pages/index.css'
+import '../styles/components/author.css'
+import '../styles/components/footer.css'
+import '../styles/pages/detail.css'
+import 'antd/dist/antd.css'
+
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
